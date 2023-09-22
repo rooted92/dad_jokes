5875d6569b9c522e5ed3e19930b61c2b65ed3a1a
pcastaneda-tfe <pcastaneda@tfewines.com>
2023-09-22 16:31:11 -0700

    created function to fetch jokes, eListener to btn

---
+++ app.js
@@ -0,0 +1,25 @@
+// API link used: https://icanhazdadjoke.com/api
+
+const jokeBtn = document.querySelector('#jokeBtn');
+const joke = document.querySelector('#joke');
+
+const generateJoke = async () => {
+    // API requires header to be set to application/json, otherwise it will return html
+    const setHeader = {
+        headers: {
+        Accept: 'application/json'
+        }
+    }
+    
+    // Fetching data from API
+    const response = await fetch('https://icanhazdadjoke.com', setHeader)
+    // Storing data in JSON format
+    const data = await response.json();
+    // Displaying data
+    joke.innerHTML = data.joke;
+}
+
+generateJoke();
+
+// Event listener
+jokeBtn.addEventListener('click', generateJoke);
